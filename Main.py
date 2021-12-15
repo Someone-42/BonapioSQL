8db450b927856dcde886ccff305cb302f35ae0ad
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2021-12-15 11:46:01 +0100

    Main base

---
+++ Main.py
@@ -0,0 +1,20 @@
+import CreateDatabase as db
+import os
+
+DATA_DIR = "./Data/"
+
+def Init():
+    if not os.path.exists(DATA_DIR):
+        os.mkdir(DATA_DIR)
+
+
+def main():
+    pass
+
+def Test1():
+    pass
+    
+
+if __name__ == "__main__":
+    Init()
+    Test1()
