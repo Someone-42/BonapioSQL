3a9f2092e6e4ed58bf28d97324c2ed89437c205f
Someone-42 <tristn.lemarchand@gmail.com>
2021-12-16 18:39:56 +0100

    Database Creation

--- Main.py
+++ Main.py
@@ -1,9 +1,14 @@
 import DBUtils as db
+import BDshell as dbs
 import os
 
+# Directories
 DATA_DIR = "./Data/"
-SQL_ORDERS_DIR = "./SQLOrders"
+SQL_ORDERS_DIR = "./SQLOrders/"
+
+# User Database
 USER_DB_NAME = DATA_DIR + "Utilisateurs.db"
+USER_DB_ORDER_FILE = SQL_ORDERS_DIR + "CreateUserDB.txt"
 
 def check_for_dir(dir: str):
     if not os.path.exists(dir):
@@ -12,12 +17,13 @@ def check_for_dir(dir: str):
 def init():
     check_for_dir(DATA_DIR)
     check_for_dir(SQL_ORDERS_DIR)
+    db.reset_db(USER_DB_NAME, USER_DB_ORDER_FILE)
 
 def main():
     pass
 
 def test1():
-    pass
+    dbs.affiche_base(USER_DB_NAME)
     
 if __name__ == "__main__":
     init()
