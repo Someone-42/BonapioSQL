4c1ee14c01697ca9acb776ab43049a4aad30de59
Someone-42 <62707457+Someone-42@users.noreply.github.com>
2022-01-04 08:51:13 +0100

    FillDB method in main
    Fills the database with randomly generated users

--- Main.py
+++ Main.py
@@ -3,6 +3,8 @@ import BDshell as dbs
 import DataGen as dg
 import os
 
+from Generator import Generator
+
 # Directories
 DATA_DIR = "./Data/"
 SQL_ORDERS_DIR = "./SQLOrders/"
@@ -10,6 +12,9 @@ SQL_ORDERS_DIR = "./SQLOrders/"
 # User Database
 USER_DB_NAME = DATA_DIR + "Utilisateurs.db"
 USER_DB_ORDER_FILE = SQL_ORDERS_DIR + "CreateUserDB.txt"
+
+# AutoInsert file
+AUTO_INSERT_ORDERS_FILE = SQL_ORDERS_DIR + "InsertOrders.txt"
 
 def check_for_dir(dir: str):
     if not os.path.exists(dir):
@@ -33,6 +38,22 @@ def test1():
 
     dbs.affiche_base(USER_DB_NAME)
 
+def fill_db(names_freq: list, first_names_freq: list, quantity: int = 42):
+    """ Inserts {quantity} amounts of random generated users in the database """
+    # Generators
+    first_name_gen = Generator(first_names_freq)
+    name_gen = Generator(names_freq)
+
+    # Creates a file containing SQL Insert orders to insert users
+    with open(AUTO_INSERT_ORDERS_FILE, "w") as f:
+        for i in range(quantity):
+            f.write(db.get_insert_order("Utilisateur", name_gen.next(), first_name_gen.next(), dg.generate_telephone()))
+    
+
+    # Reads the file and executes its orders
+    dbs.execute_fichier(USER_DB_NAME, AUTO_INSERT_ORDERS_FILE)
+
 if __name__ == "__main__":
     init()
-    test1()
+    # fill_db()
+    dbs.affiche_base()
